efcd9a5c82d52ccd1e0e8909d8d059418d720d54
Maks <maks@tarasutin.ru>
2023-11-17 20:47:44 +0300

    Обновил template

--- wbnotifierbot/templates.py
+++ wbnotifierbot/templates.py
@@ -5,7 +5,7 @@ import jinja2
 from wbnotifierbot import config
 
 
-def render_template(template_name: str, data: dict | None = None) -> str:
+def render_template(template_name: str, data: dict = None) -> str:
     if data is None:
         data = {}
     template = _get_template_env().get_template(template_name)
