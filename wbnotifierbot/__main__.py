1b929429686be98457e44666eecd1e7cff167231
Maks <maks@tarasutin.ru>
2023-11-17 20:12:21 +0300

    first commit

---
+++ wbnotifierbot/__main__.py
@@ -0,0 +1,49 @@
+import logging
+
+from telegram.ext import (
+    ApplicationBuilder,
+    CallbackQueryHandler,
+    CommandHandler,
+    MessageHandler,
+    filters,
+)
+
+from wbnotifierbot import config, handlers
+from wbnotifierbot.db import close_db
+
+COMMAND_HANDLERS = {
+    "start": handlers.start,
+    "help": handlers.help_,
+}
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+
+if not config.TELEGRAM_BOT_TOKEN:
+    raise ValueError(
+        "env переменная TELEGRAM_BOT_TOKEN  "
+        "не объявлена в .env (должна быть!)"
+    )
+
+
+def main():
+    application = ApplicationBuilder().token(config.TELEGRAM_BOT_TOKEN).build()
+
+    for command_name, command_handler in COMMAND_HANDLERS.items():
+        application.add_handler(CommandHandler(command_name, command_handler))
+
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception:
+        import traceback
+
+        logger.warning(traceback.format_exc())
+    finally:
+        close_db()
---
+++ wbnotifierbot/handlers/start.py
@@ -0,0 +1,19 @@
+
+from telegram import Chat, Update
+from telegram.ext import ContextTypes
+from typing import cast
+from wbnotifierbot.handlers.response import send_response
+from wbnotifierbot.service.check_user_is_admin import is_user_admin
+from wbnotifierbot.templates import render_template
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if is_user_admin(cast(Chat, update.effective_chat).id:
+        await send_response(update, context, 
+                            response=render_template(
+                                            "start_4_admin.j2", 
+                                             data={"name":update.message.from_user.username}
+                            )
+        )
+    else:
+        await send_response(update, context, response=render_template("start.j2"))
