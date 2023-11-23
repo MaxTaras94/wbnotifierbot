3b2fa91f559610b06e66f38a5013a5b7f165edfd
Maks <maks@tarasutin.ru>
2023-11-23 15:12:12 +0300

    Поправил проверку пользователей в БД

--- wbnotifierbot/handlers/start.py
+++ wbnotifierbot/handlers/start.py
@@ -10,11 +10,11 @@ from wbnotifierbot.templates import render_template
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reply_keyboard = [["Обновить данные🔁"]]
     reply_markup=ReplyKeyboardMarkup(reply_keyboard, is_persistent=True, resize_keyboard=True)
-    if is_user_admin(cast(Chat, update.effective_chat).id):
+    if await is_user_admin(cast(Chat, update.effective_chat).id) == 1:
         await send_response(response=render_template(
                                             "start_4_admin.j2", 
                                              data={"name":update.message.from_user.username}
                             ),update=update, context=context, reply_keyboard=reply_markup
         )
     else:
-        await send_response(update, context, response=render_template("start.j2", data={"name":update.message.from_user.username}) )
+        await send_response(response=render_template("start.j2", data={"name":update.message.from_user.username}), update=update, context=context )
